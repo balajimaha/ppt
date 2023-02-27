2514ac96d8b04a3e74db9a36e6d7cd7d794f7f31
Yevhenii Shudrenko <yevhenii.shudrenko@tuhh.de>
2023-02-27 12:56:07 +0100

    Put the option to customize .tex and .bib filenames back

--- copy_themes_files.py
+++ copy_themes_files.py
@@ -28,15 +28,23 @@ parser.add_argument(
     'destPath', 
     help='path of the folder where to copy the files (can be relative)')
 
+parser.add_argument(
+    '--texSlidesName', default=None,
+    help='if provided, also copy the tuhh template into a file with given name')
+
+parser.add_argument(
+    '--bibFileName', default='references.bib',
+    help='if texSlidesName is given, copy the bibtex template to the following name')
+
+parser.add_argument(
+    '--includeTemplate', type=bool, default=False, const=True, nargs='?',
+    help='if set, copy template .tex, .bib files and name them after the dest directory')
+
 args = parser.parse_args()
 
 dest = args.destPath
 if not os.path.isdir(dest):
     raise ValueError(f'{dest} is not a folder, please create it before')
-
-destFolder = os.path.basename(os.path.normpath(args.destPath))
-texSlidesName = f'{destFolder}.tex'
-bibFileName = f'{destFolder}.bib'
 
 for file in listFiles:
     shutil.copy(file, os.path.join(dest, file))
@@ -45,20 +53,36 @@ if os.path.isdir(themeDest):
     shutil.rmtree(themeDest)
 shutil.copytree(themeDir, themeDest)
 
-with open(templateTexName, 'r') as f:
-    tex = f.read()
-tex = tex.replace(templateBibName, bibFileName)
+if args.includeTemplate:
+    destFolder = os.path.basename(os.path.normpath(args.destPath))
+    texSlidesName = f'{destFolder}.tex'
+    bibFileName = f'{destFolder}.bib'
 
-if os.path.isfile(os.path.join(dest, texSlidesName)):
-    print('.tex file already exists') 
-else:
-    with open(os.path.join(dest, texSlidesName), 'w') as f:
-        f.write(tex)
+    with open(templateTexName, 'r') as f:
+        tex = f.read()
+    tex = tex.replace(templateBibName, bibFileName)
 
-if os.path.isfile(os.path.join(dest, bibFileName)):
-    print('bib file already exists')
+    if os.path.isfile(os.path.join(dest, texSlidesName)):
+        print('.tex file already exists') 
+    else:
+        with open(os.path.join(dest, texSlidesName), 'w') as f:
+            f.write(tex)
+
+    if os.path.isfile(os.path.join(dest, bibFileName)):
+        print('bib file already exists')
+    else:
+        shutil.copy(templateBibName, os.path.join(dest, bibFileName))
+
 else:
-    shutil.copy(templateBibName, os.path.join(dest, bibFileName))
+    if args.texSlidesName is not None:
+        with open(templateTexName, 'r') as f:
+            tex = f.read()
+        tex = tex.replace(templateBibName, args.bibFileName)
+        with open(os.path.join(dest, args.texSlidesName), 'w') as f:
+            f.write(tex)
+        shutil.copy(templateBibName, os.path.join(dest, args.bibFileName))
+
+
     
     
 
